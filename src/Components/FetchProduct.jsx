4d047b6f3040e23318ab5a0356ec4c32412bb4ac
Isreality <isrealitycreations@gmail.com>
2024-07-05 18:39:43 +0100

    delete btn

--- src/Components/FetchProduct.jsx
+++ src/Components/FetchProduct.jsx
@@ -1,6 +1,7 @@
 import "../style.css";
 import { useAuth } from '../Components/AuthContext';
 import { useState, useEffect } from 'react';
+import { HiOutlineTrash } from "react-icons/hi";
 
 const FetchProduct = () => {
   const { authToken, setStatusCode } = useAuth();
@@ -49,12 +50,18 @@ const FetchProduct = () => {
     <div>
       <div className="">
                 {data.map((open) => (
-                    <div key={open.id} className="flex flex-row gap-5 items-center border-b border-disable p-4">
+                    <div key={open.id} className="flex flex-row justify-between items-center border-b border-disable p-4">
+                      <div className="flex flex-row gap-5 items-center p-4">
                         <div><img src={open.imageUrl} alt="" className="h-10 w-10 md:h-16 md:w-16 rounded-md"/></div>
                         <div className="">
                             <h1 className="text-sm md:text-md text-left text-black2 font-medium">{open.name}</h1>
                             <h1 className="text-sm text-left text-black2 font-normal">N{open.price}</h1>    
                         </div>
+                      </div>
+
+                      <div>
+                        <HiOutlineTrash className="text-red size-6 cursor-pointer"/>
+                      </div>
                     </div> 
                 ))}
       </div>
